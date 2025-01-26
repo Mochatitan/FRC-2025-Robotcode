f4e4505936abf0504fd8330bfd8f6033e7662699
Lincoln <lincoln@vulpes.com>
2025-01-26 14:31:24 -0500

    Swerve(May Work??????)
    Added in Module Constants(Will need to change)
    Added Driver's deadband(Number was pulled out of air)
    Commented out old Auto driver(Will need this for the future but for now we don't need it)
    Changed the .getMaximum(x) to .getMaximumChassis(x)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,9 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -15,5 +18,27 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+    public static final double driveDeadband = 0.05;
   }
+
+  public static class ModuleConstants {
+        // The max free speed of the module
+        public static final double maxSpeed = 4.5;
+        //Lowerd from 4.5
+
+        public static double trackWidth = 0.5588;
+        public static double wheelBase = 0.4826;
+
+        public static Translation2d frontLeftPosition = new Translation2d(trackWidth/2,wheelBase/2);
+        public static Translation2d frontRightPosition = new Translation2d(-trackWidth/2,wheelBase/2);
+        public static Translation2d backLeftPosition = new Translation2d(trackWidth/2,-wheelBase/2);
+        public static Translation2d backRightPosition = new Translation2d(-trackWidth/2,-wheelBase/2);
+
+        public static SwerveDriveKinematics driveKinematics = new SwerveDriveKinematics(frontLeftPosition,
+                                                                                frontRightPosition,
+                                                                                backLeftPosition,
+                                                                                backRightPosition);
+
+      }
+
 }
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -8,10 +8,9 @@ import java.io.File;
 import java.io.IOException;
 import java.util.function.DoubleSupplier;
 
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.auto.CommandUtil;
 import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
-import com.pathplanner.lib.util.PIDConstants;
-import com.pathplanner.lib.util.ReplanningConfig;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -67,32 +66,32 @@ public class DriveSubsystem extends SubsystemBase {
       DriverStation.reportError("Error with loading config: ", exception.getStackTrace());
     }
 
-    AutoBuilder.configureHolonomic(
-            this::getPose, // Robot pose supplier
-            this::resetPose, // Method to reset odometry (will be called if your auto has a starting pose)
-            this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
-            this::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
-            new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
-                    new PIDConstants(4, 0.0, 0.0), // Translation PID constants
-                    new PIDConstants(4, 0.0, 0.0), // Rotation PID constants
-                    ModuleConstants.maxSpeed, // Max module speed, in m/s
-                    0.356, // Drive base radius in meters. Distance from robot center to furthest module.
+    // AutoBuilder.configureHolonomic(
+    //         this::getPose, // Robot pose supplier
+    //         this::resetPose, // Method to reset odometry (will be called if your auto has a starting pose)
+    //         this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+    //         this::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+    //         new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
+    //                 new PIDConstants(4, 0.0, 0.0), // Translation PID constants
+    //                 new PIDConstants(4, 0.0, 0.0), // Rotation PID constants
+    //                 ModuleConstants.maxSpeed, // Max module speed, in m/s
+    //                 0.356, // Drive base radius in meters. Distance from robot center to furthest module.
                 
-                    new ReplanningConfig() // Default path replanning config. See the API for the options here
-            ),
-            () -> {
-              // Boolean supplier that controls when the path will be mirrored for the red alliance
-              // This will flip the path being followed to the red side of the field.
-              // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+    //                 new ReplanningConfig() // Default path replanning config. See the API for the options here
+    //         ),
+    //         () -> {
+    //           // Boolean supplier that controls when the path will be mirrored for the red alliance
+    //           // This will flip the path being followed to the red side of the field.
+    //           // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
 
-              var alliance = DriverStation.getAlliance();
-              if (alliance.isPresent()) {
-                return alliance.get() == DriverStation.Alliance.Red;
-              }
-              return false;
-            },
-            this // Reference to this subsystem to set requirements
-    );
+    //           var alliance = DriverStation.getAlliance();
+    //           if (alliance.isPresent()) {
+    //             return alliance.get() == DriverStation.Alliance.Red;
+    //           }
+    //           return false;
+    //         },
+    //         this // Reference to this subsystem to set requirements
+    // );
     SmartDashboard.putData("Field",m_field);
   }
 
@@ -116,9 +115,9 @@ public class DriveSubsystem extends SubsystemBase {
       y = Math.pow(y, 3);
       double angle = -MathUtil.applyDeadband(angularRotationX.getAsDouble(), OperatorConstants.driveDeadband);
       angle = Math.pow(angle,3);
-      swerveDrive.drive(new Translation2d(y * swerveDrive.getMaximumVelocity()/4,
-                        x * swerveDrive.getMaximumVelocity()/4),
-                        angle * swerveDrive.getMaximumAngularVelocity()/2,
+      swerveDrive.drive(new Translation2d(y * swerveDrive.getMaximumChassisVelocity()/4,
+                        x * swerveDrive.getMaximumChassisVelocity()/4),
+                        angle * swerveDrive.getMaximumChassisAngularVelocity()/2,
                         true,
                         false);
     });
